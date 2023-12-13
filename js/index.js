1caee937edd2633a4e46a8f76fb0dabd12e0b430
BearHubs <bear90789078@gmail.com>
2023-12-13 17:15:37 +0800

    20231213

--- js/index.js
+++ js/index.js
@@ -10,6 +10,154 @@ index = (function () {
     rcasc: rcasc
   };
   function init() {
+    window.addEventListener('resize', function () {
+      const navHeight = document.querySelector('.nav').offsetHeight;
+      document.querySelector('.home').style.marginTop = (navHeight + 30) + 'px';
+    });
+
+    document.addEventListener("selectionchange", () => {
+      if (!isStrong()) {
+        document.querySelector('.tools-blod').classList.remove('tools-active');
+      } else {
+        document.querySelector('.tools-blod').classList.add('tools-active');
+      }
+    });
+
+    document.querySelectorAll('.comment-tools > *').forEach(c => {
+      c.addEventListener('click', function () {
+        document.querySelectorAll('.comment-tools > *').forEach(otherc => {
+          if (otherc !== this) {
+            //otherc.classList.remove('tools-active');
+          }
+        });
+        this.classList.remove('tools-hover');
+        this.classList.toggle('tools-active');
+        if (this.classList.contains('tools-blod') && this.classList.contains('tools-active')) {
+          if (!document.querySelector('#comment').contains(document.querySelector("strong"))) {
+            document.querySelector('#comment').appendChild(document.createElement("strong"));
+          }
+          moveTextInsideStrong();
+        } else if (this.classList.contains('tools-blod') && !this.classList.contains('tools-active')) {
+          moveTextOutsideStrong();
+        }
+      });
+      c.addEventListener('mousedown', function (e) {
+        e.preventDefault();
+      });
+      c.addEventListener('mouseenter', function () {
+        if (!this.classList.contains('tools-active')) {
+          this.classList.toggle('tools-hover');
+        }
+      });
+      c.addEventListener('mouseleave', function () {
+        this.classList.remove('tools-hover');
+      });
+    });
+    function moveTextInsideStrong() {
+      let selection = window.getSelection();
+      if (selection.rangeCount > 0) {
+        let range = selection.getRangeAt(0);
+        let selectedText = range.toString();
+        let startNode = range.startContainer;
+        let endNode = range.endContainer;
+
+        let strongElement = document.createElement('strong');
+        strongElement.textContent = selectedText;
+
+        // Extract the selected text
+        range.deleteContents();
+        range.insertNode(strongElement);
+
+        // Re-select the original range
+        let newRange = document.createRange();
+        if (startNode.nodeType === Node.TEXT_NODE) {
+          newRange.setStart(strongElement.firstChild, 0);
+          newRange.setEnd(strongElement.firstChild, selectedText.length);
+        } else {
+          newRange.selectNodeContents(strongElement);
+        }
+
+        selection.removeAllRanges();
+        selection.addRange(newRange);
+
+        document.querySelector('#comment').childNodes.forEach(strong => {
+          if (strong.innerText === "" || null) {
+            strong.remove();
+          }
+        });
+      }
+    }
+    function moveTextOutsideStrong() {
+      let selection = window.getSelection();
+      if (selection.rangeCount > 0) {
+        let range = selection.getRangeAt(0);
+
+        let selectedText = range.toString();
+        let strongElement = document.querySelector('#comment strong');
+
+        let strongRange = document.createRange();
+        strongRange.selectNode(strongElement);
+
+        if (strongRange.compareBoundaryPoints(Range.START_TO_START, range) <= 0 && strongRange.compareBoundaryPoints(Range.END_TO_END, range) >= 0) {
+          let parent = strongElement.parentNode;
+          let content = strongElement.innerHTML;
+
+          let startOffset = range.startOffset;
+          let endOffset = range.endOffset;
+
+          let beforeContent = content.substring(0, startOffset);
+          let selectedContent = content.substring(startOffset, endOffset);
+          let afterContent = content.substring(endOffset);
+
+          if (beforeContent !== '') {
+            let beforeStrong = document.createElement('strong');
+            beforeStrong.innerHTML = beforeContent;
+            parent.insertBefore(beforeStrong, strongElement);
+          }
+
+          if (selectedContent !== '') {
+            let selectedStrong = document.createElement('span');
+            selectedStrong.innerHTML = selectedContent;
+            parent.insertBefore(selectedStrong, strongElement);
+            var spanToRemove = document.getElementById('comment').querySelector('span');
+            var textNode = document.createTextNode(spanToRemove.textContent);
+            var parentElement = spanToRemove.parentElement;
+            parentElement.replaceChild(textNode, spanToRemove);
+          }
+
+          if (afterContent !== '') {
+            strongElement.innerHTML = afterContent;
+          } else {
+            parent.removeChild(strongElement);
+          }
+
+          range.deleteContents();
+        } else {
+          console.log('Selection includes whole strong element');
+        }
+      }
+    }
+    function isStrong() {
+      let selection = window.getSelection();
+      if (selection.rangeCount > 0) {
+        let range = selection.getRangeAt(0);
+        let startNode = range.startContainer;
+        let endNode = range.endContainer;
+
+        let strongElements = document.querySelectorAll('#comment strong');
+
+        for (let i = 0; i < strongElements.length; i++) {
+          const strongElement = strongElements[i];
+          if (strongElement) {
+            let isInStrong = strongElement.contains(startNode) && strongElement.contains(endNode);
+            if (isInStrong) {
+              return true;
+            }
+          }
+        }
+      }
+      return false;
+    }
     const pbtn = document.getElementById("btn-status");
     pbtn.addEventListener("click", () => {
       if (!active) {

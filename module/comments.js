05d9caa8b5c9eb6c18b5e29559982a99c2f946fd
BearHubs <bear90789078@gmail.com>
2024-12-14 10:24:08 +0800

    Update StartChange
    Change Version

---
+++ module/comments.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, child, get, set, update, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+const comments = (() => {
+    return {
+        init: () => {
+            
+        }
+    }
+})();

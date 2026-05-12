f8a70956b2c46abbd4092c24b46db5f7521bc235
duckodes <bear90789078@gmail.com>
2026-05-12 15:02:43 +0800

    update image compression

--- module/generatecard.js
+++ module/generatecard.js
@@ -3,9 +3,9 @@ const generatecard = (() => {
         init: async (languageData) => {
             const cardarea = document.querySelector('.card-area');
             cardarea.innerHTML = '';
-            gcard(cardarea, languageData.card.items.research, "img/windowgame(unity)/windowgame(unity)_1024x_72ppi.png", "https://notes.duckode.com", languageData);
+            gcard(cardarea, languageData.card.items.research, "img/windowgame(unity)/windowgame(unity)_1024x_72ppi.jpg", "https://notes.duckode.com", languageData);
             gcard(cardarea, languageData.card.items.jslibrary, "img/research/research_500x500_72ppi.png", "https://lib.duckode.com", languageData);
-            gcard(cardarea, languageData.card.items.library, "img/csharplib/topic-csharplib_1024x_144ppi.png", "https://topic-csharplib.duckode.com", languageData);
+            gcard(cardarea, languageData.card.items.library, "img/csharplib/topic-csharplib_1024x_144ppi.jpg", "https://topic-csharplib.duckode.com", languageData);
         }
     }
     async function gcard(cardarea, t, i, l, languageData) {
